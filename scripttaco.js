0cb27451b5ea835a430d37bd90ef923c2f9e8055
Ben Honken <honkenb@gmail.com>
2019-10-23 20:54:17 -0500

    linked script to html, added taco DOM updater, fixed event listeners

--- scripttaco.js
+++ scripttaco.js
@@ -4,9 +4,8 @@ var baseLayer = "";
 var seasoning = "";
 var mixin = "";
 var condiment = "";
-
-//add on click effect for whole taco button and each layer
-$("#tacoBout").on("click", function(){
+tacoDisplay();
+$("#tacoHeader").on("click", function(){
     $.ajax({
         url: queryURL,
         type: "GET",
@@ -16,29 +15,30 @@ $("#tacoBout").on("click", function(){
                 seasoning = response.seasoning;
                 mixin = response.mixin;
                 condiment = response.condiment;
+                $("#shellName").text(shell.name);
+                $("#shellRecipe").text(shell.recipe);
+                $("#baseName").text(baseLayer.name);
+                $("#baseRecipe").text(baseLayer.recipe);
+                $("#seasoningName").text(seasoning.name);
+                $("#seasoningRecipe").text(seasoning.recipe);
+                $("#mixinName").text(mixin.name);
+                $("#mixinRecipe").text(mixin.recipe);
+                $("#condimentName").text(condiment.name);
+                $("#condimentRecipe").text(condiment.recipe);
             }
       })
-    $("#shellName").text(shell.name);
-    $("#shellRecipe").text(shell.recipe);
-    $("#baseName").text(baseLayer.name);
-    $("#baseRecipe").text(baseLayer.recipe);
-    $("#seasoningName").text(seasoning.name);
-    $("#seasoningRecipe").text(seasoning.recipe);
-    $("#mixinName").text(mixin.name);
-    $("#mixinRecipe").text(mixin.recipe);
-    $("#condimentName").text(condiment.name);
-    $("#condimentRecipe").text(condiment.recipe);
 })
 $("#newShell").on("click", function(){
+    console.log("hello");
     $.ajax({
         url: queryURL,
         type: "GET",
             success: function(response){
                 shell = response.shell;
+                $("#shellName").text(shell.name);
+                $("#shellRecipe").text(shell.recipe);
             }
       })
-    $("#shellName").text(shell.name);
-    $("#shellRecipe").text(shell.recipe);
 })
 $("#newBase").on("click", function(){
     $.ajax({
@@ -46,10 +46,10 @@ $("#newBase").on("click", function(){
         type: "GET",
             success: function(response){
                 baseLayer = response.base_layer;
+                $("#baseName").text(baseLayer.name);
+                $("#baseRecipe").text(baseLayer.recipe);
             }
       })
-    $("#baseName").text(baseLayer.name);
-    $("#baseRecipe").text(baseLayer.recipe);
 })
 $("#newSeasoning").on("click", function(){
     $.ajax({
@@ -57,10 +57,10 @@ $("#newSeasoning").on("click", function(){
         type: "GET",
             success: function(response){
                 seasoning = response.seasoning;
+                $("#seasoningName").text(seasoning.name);
+                $("#seasoningRecipe").text(seasoning.recipe);
             }
       })
-    $("#seasoningName").text(seasoning.name);
-    $("#seasoningRecipe").text(seasoning.recipe);
 })
 $("#newMixin").on("click", function(){
     $.ajax({
@@ -68,10 +68,10 @@ $("#newMixin").on("click", function(){
         type: "GET",
             success: function(response){
                 mixin = response.mixin;
+                $("#mixinName").text(mixin.name);
+                $("#mixinRecipe").text(mixin.recipe);
             }
       })
-    $("#mixinName").text(mixin.name);
-    $("#mixinRecipe").text(mixin.recipe);
 })
 $("#newCondiment").on("click", function(){
     $.ajax({
@@ -79,8 +79,35 @@ $("#newCondiment").on("click", function(){
         type: "GET",
             success: function(response){
                 condiment = response.condiment;
+                $("#condimentName").text(condiment.name);
+                $("#condimentRecipe").text(condiment.recipe);
             }
       })
-    $("#condimentName").text(condiment.name);
-    $("#condimentRecipe").text(condiment.recipe);
 })
+function tacoDisplay(){
+    $("#tacoBox").append($("<div id='shellDiv'>"));
+    $("#shellDiv").append($("<div id='shellName'>"));
+    $("#shellDiv").append($("<div id='shellRecipe'>"));
+    $("#shellDiv").append($("<button class='btn' id='newShell'>"));
+    $("#newShell").text("New Shell");
+    $("#tacoBox").append($("<div id='baseDiv'>"));
+    $("#baseDiv").append($("<div id='baseName'>"));
+    $("#baseDiv").append($("<div id='baseRecipe'>"));
+    $("#baseDiv").append($("<button class='btn' id='newBase'>"));
+    $("#newBase").text("New Base");
+    $("#tacoBox").append($("<div id='seasoningDiv'>"));
+    $("#seasoningDiv").append($("<div id='seasoningName'>"));
+    $("#seasoningDiv").append($("<div id='seasoningRecipe'>"));
+    $("#seasoningDiv").append($("<button class='btn' id='newSeasoning'>"));
+    $("#newSeasoning").text("New Seasoning");
+    $("#tacoBox").append($("<div id='mixinDiv'>"));
+    $("#mixinDiv").append($("<div id='mixinName'>"));
+    $("#mixinDiv").append($("<div id='mixinRecipe'>"));
+    $("#mixinDiv").append($("<button class='btn' id='newMixin'>"));
+    $("#newMixin").text("New Mixin");
+    $("#tacoBox").append($("<div id='condimentDiv'>"));
+    $("#condimentDiv").append($("<div id='condimentName'>"));
+    $("#condimentDiv").append($("<div id='condimentRecipe'>"));
+    $("#condimentDiv").append($("<button class='btn' id='newCondiment'>"));
+    $("#newCondiment").text("New Condiment");
+}
