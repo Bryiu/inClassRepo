d3668f2ff77cef89fbc2c6de68d778c2e6b102c0
Bryan Iund <bwiund0823@gmail.com>
2019-11-01 16:30:42 -0500

    second screen of buttons working

--- scriptdrink.js
+++ scriptdrink.js
@@ -1,6 +1,8 @@
 var proxy = "http://www.chriscastle.com/proxy/thecocktaildb.php?:proxy:";
 var ingredient = JSON.parse(localStorage.getItem("alcohol"));
-console.log(ingredient);
+var drinkSearch = $("#tequilaBox");
+var drinkArray = [];
+var buttonArray = [];
 var queryURL1 = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
 function searchDiv() {
     var search = $("<div>")
@@ -11,54 +13,34 @@ function searchDiv() {
     textarea.attr("cols", "54");
     textarea.attr("rows", "2");
     search.append(textarea);
-    var button1 = $("<button class='animated delay pulse' id='searchDrink'>");
+    var button1 = $("<button id='searchDrink'>");
     button1.html("Search");
     search.append(button1);
-    $("#tequilaBox").html(search);
+    $(drinkSearch).html(search);
 };
 searchDiv();
 
 $(document).on("click", "#searchDrink", function () {
-    event.preventDefault()
-    console.log($("#textarea").val().trim());
-    var drink = $("#textarea").val().trim();
-    if (drinkArray.indexOf(drink) === -1) {
-        drinkArray.concat(drink);
-        localStorage.setItem("alcohol", JSON.stringify(drink));
+    event.preventDefault();
+    var drink1 = $("#textarea").val().trim() || "";
+    if (drinkArray.indexOf(drink1) === -1) {
+        drinkArray.push(drink1);
+        localStorage.setItem("alcohol", JSON.stringify(drink1));
     }
     $("#searchDiv").html("");
-
     $.ajax({
         url: queryURL1,
         method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        var randomNumber = Math.floor((Math.random() * response.drinks.length) + 0);
-        drinkName = response.drinks[randomNumber].strDrink;
-
-        console.log(drinkName);
+    }).then(function (response){
+        for(i = 0; i < 5; i++){
+            var randomNumber1 = Math.floor((Math.random() * 17) + 0);
+            drinkName = response.drinks[randomNumber1].strDrink;
+            buttonArray.push(drinkName);
+            console.log(buttonArray);
+            var button2 = $("<button id='searchDrink'>");
+            button2.html(buttonArray[i]);
+            drinkSearch.append(button2);
+        }
     });
 });
 
-function populate2() {
-    var button2 = $("<button>");
-    button2.html(drinks[randomNumber].strDrink);
-    button2.append(drinkSearch);
-    $("#tequilaBox").append(button2);
-}
-
-
-populate2();
-
-var drinkSearch = $("#tequilaBox");
-var drinkArray = [];
-
-
-
-
-
-
-// Add a search button and push it to the box
-// have the user search by alcohol
-// use the string from the search bar to search the API
-// pull
